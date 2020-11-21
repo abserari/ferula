8bc85fc5f6cc28a087c1ac2ac51e8ab28c51a6a8
AbserAri <abserari@gmail.com>
2020-11-21 22:08:56 +0800

    Update main.go

--- minio/main/main.go
+++ minio/main/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"log"
+"context"
+"log"
 
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
+"github.com/minio/minio-go/v7"
+"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 func main() {
+	ctx := context.Background()
 	endpoint := "39.105.141.168:9000"
 	accessKeyID := "minioadmin"
 	secretAccessKey := "minioadmin"
@@ -22,5 +24,33 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	log.Printf("%#v\n", minioClient) // minioClient is now set up
+	// Make a new bucket called mymusic.
+	bucketName := "mymusic"
+	location := "us-east-1"
+
+	err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("We already own %s\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Printf("Successfully created %s\n", bucketName)
+	}
+
+	// Upload the zip file
+	objectName := "golden-oldies.zip"
+	filePath := "/tmp/golden-oldies.zip"
+	contentType := "application/zip"
+
+	// Upload the zip file with FPutObject
+	n, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", objectName, n)
 }
