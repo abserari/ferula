4db9124cd1c5d2060576be4f6ce6ae07ee202a5d
AbserAri <abserari@gmail.com>
2020-11-21 16:37:52 +0800

    init with shower

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/abserari/ferula
+
+go 1.15
+
+require (
+	github.com/abserari/shower v0.0.0-20201017024050-4cc9ce467c77
+	github.com/appleboy/gin-jwt/v2 v2.6.4
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/minio/minio-go/v7 v7.0.5
+	github.com/sfreiberg/gotwilio v0.0.0-20200916182813-169c4cd5c691
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9
+)
---
+++ minio/main/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func main() {
+	endpoint := "39.105.141.168:9000"
+	accessKeyID := "minioadmin"
+	secretAccessKey := "minioadmin"
+	useSSL := true
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("%#v\n", minioClient) // minioClient is now set up
+}
