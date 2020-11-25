ab063b3a4cc8ab2127be2de352d836abbb2f476b
AbserAri <abserari@gmail.com>
2020-11-25 13:45:12 +0800

    update

---
+++ admin/src/pages/Forms/basicForm.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import {
+    Button,
+    Card,
+    Checkbox,
+    Form,
+    Input,
+} from 'antd';
+import { PageContainer } from '@ant-design/pro-layout';
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+};
+
+const onFinish = (values: any) => {
+    console.log('Success:', values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+};
+
+export default (): React.ReactNode => (
+    <PageContainer>
+        <Card>
+            <Form
+                {...layout}
+                name="basic"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Card>
+    </PageContainer>
+)
--- smallprogram/bootstrap.go
+++
@@ -1,3 +0,0 @@
-package main
-import _ "github.com/GoAdminGroup/filemanager"
-import _ "github.com/GoAdminGroup/librarian"
